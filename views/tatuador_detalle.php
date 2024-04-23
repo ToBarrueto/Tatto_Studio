b5efd4dbf129bce73e5f3c1995dbdb641b88129f
Tomas Barrueto <134245189+ToBarrueto@users.noreply.github.com>
2024-04-23 00:25:55 -0400

    Se crea primer intento de procesar reserva, aun no funciona aun un error con el id del tatuador.

--- views/tatuador_detalle.php
+++ views/tatuador_detalle.php
@@ -91,115 +91,118 @@ if (isset($_GET['id'])) {
 
              // Formulario de reserva de citas
            
-            echo '<div class="card col-8 ms-5">';
-            echo '<div class="card-body">';
-            echo '<h2>Reservar Cita</h2>';
-            echo '<form action="procesar_reserva.php" method="POST" enctype="multipart/form-data">';
-            echo '<input type="hidden" name="tatuador_id" value="' . $tatuador_id . '">';
+echo '<div class="card col-8 ms-5">';
+echo '<div class="card-body">';
+echo '<h2>Reservar Cita</h2>';
+echo '<form action="procesar_reserva.php" method="POST" enctype="multipart/form-data">';
+echo '<input type="hidden" name="tatuador_id" value="' . $tatuador_id . '">';
 
-            // Campos adicionales
-            echo '<div class="row">';
-            echo '<div class="col-md-6">';
-            echo '<div class="mb-3">';
-            echo '<label for="nombre_cliente" class="form-label">Nombre Completo:</label>';
-            echo '<input type="text" class="form-control" id="nombre_cliente" name="nombre_cliente" required>';
-            echo '</div>';
+// Campo de ID de cliente
+echo '<input type="hidden" name="cliente_id" value="' . $_SESSION['usuario_id'] . '">';
 
-            echo '<div class="mb-3">';
-            echo '<label for="telefono" class="form-label">Número de Celular:</label>';
-            echo '<input type="tel" class="form-control" id="telefono" name="telefono" required>';
-            echo '</div>';
+// Campos adicionales
+echo '<div class="row">';
+echo '<div class="col-md-6">';
+echo '<div class="mb-3">';
+echo '<label for="nombre_cliente" class="form-label">Nombre Completo:</label>';
+echo '<input type="text" class="form-control" id="nombre_cliente" name="nombre_cliente" required>';
+echo '</div>';
 
-            echo '<div class="mb-3">';
-            echo '<label for="correo" class="form-label">Correo Electrónico:</label>';
-            echo '<input type="email" class="form-control" id="correo" name="correo" required>';
-            echo '</div>';
+echo '<div class="mb-3">';
+echo '<label for="telefono" class="form-label">Número de Celular:</label>';
+echo '<input type="tel" class="form-control" id="telefono" name="telefono" required>';
+echo '</div>';
 
-            echo '<div class="mb-3">';
-            echo '<label for="imagen_referencia" class="form-label">Imagen de Referencia:</label>';
-            echo '<input type="file" class="form-control" id="imagen_referencia" name="imagen_referencia">';
-            echo '</div>';
-            echo '</div>';
+echo '<div class="mb-3">';
+echo '<label for="correo" class="form-label">Correo Electrónico:</label>';
+echo '<input type="email" class="form-control" id="correo" name="correo" required>';
+echo '</div>';
 
-            echo '<div class="col-md-6">';
-            echo '<div class="mb-3">';
-            echo '<label for="alto" class="form-label">Alto del Tatuaje (cm):</label>';
-            echo '<input type="number" class="form-control" id="alto" name="alto" required>';
-            echo '</div>';
+echo '<div class="mb-3">';
+echo '<label for="imagen_referencia" class="form-label">Imagen de Referencia:</label>';
+echo '<input type="file" class="form-control" id="imagen_referencia" name="imagen_referencia">';
+echo '</div>';
+echo '</div>';
 
-            echo '<div class="mb-3">';
-            echo '<label for="ancho" class="form-label">Ancho del Tatuaje (cm):</label>';
-            echo '<input type="number" class="form-control" id="ancho" name="ancho" required>';
-            echo '</div>';
+echo '<div class="col-md-6">';
+echo '<div class="mb-3">';
+echo '<label for="alto" class="form-label">Alto del Tatuaje (cm):</label>';
+echo '<input type="number" class="form-control" id="alto" name="alto" required>';
+echo '</div>';
 
-            echo '<div class="mb-3">';
-            echo '<label for="color" class="form-label">Color:</label>';
-            echo '<select class="form-select" id="color" name="color">';
-            echo '<option value="si">Con color</option>';
-            echo '<option value="no">Sin color</option>';
-            echo '</select>';
-            echo '</div>';
+echo '<div class="mb-3">';
+echo '<label for="ancho" class="form-label">Ancho del Tatuaje (cm):</label>';
+echo '<input type="number" class="form-control" id="ancho" name="ancho" required>';
+echo '</div>';
 
-            echo '<div class="mb-3">';
-            echo '<label for="parte_cuerpo" class="form-label">Zona:</label>';
-            echo '<select class="form-select" id="parte_cuerpo" name="parte_cuerpo">';
-            echo '<option value="Tronco-Espalda">Tronco-Espalda</option>';
-            echo '<option value="Cuello">Cuello</option>';
-            echo '<option value="Brazos-Manos">Brazos-Manos</option>';
-            echo '<option value="Piernas">Piernas</option>';
-            echo '<option value="Cara-Cabeza">Cara-Cabeza</option>';
-            echo '</select>';
-            echo '</div>';
+echo '<div class="mb-3">';
+echo '<label for="color" class="form-label">Color:</label>';
+echo '<select class="form-select" id="color" name="color">';
+echo '<option value="si">Con color</option>';
+echo '<option value="no">Sin color</option>';
+echo '</select>';
+echo '</div>';
 
-            echo '<div class="mb-3">';
-            echo '<label for="cantidad_sesiones" class="form-label">Cantidad de Sesiones:</label>';
-            echo '<input type="number" class="form-control" id="cantidad_sesiones" name="cantidad_sesiones" min="1" max="5" required>';
-            echo '</div>';
+echo '<div class="mb-3">';
+echo '<label for="zona" class="form-label">Zona:</label>';
+echo '<select class="form-select" id="zona" name="zona">';
+echo '<option value="Tronco-Espalda">Tronco-Espalda</option>';
+echo '<option value="Cuello">Cuello</option>';
+echo '<option value="Brazos-Manos">Brazos-Manos</option>';
+echo '<option value="Piernas">Piernas</option>';
+echo '<option value="Cara-Cabeza">Cara-Cabeza</option>';
+echo '</select>';
+echo '</div>';
 
-            echo '<div class="mb-3">';
-            echo '<label for="cotizacion" class="form-label">Cotización:</label>';
-            echo '<span id="cotizacion_aproximada"></span>';
-            echo '</div>';
-            echo '</div>';
-            
-            
-            echo '</div>';
-            echo '</div>';
+echo '<div class="mb-3">';
+echo '<label for="cantidad_sesiones" class="form-label">Cantidad de Sesiones:</label>';
+echo '<input type="number" class="form-control" id="cantidad_sesiones" name="cantidad_sesiones" min="1" max="5" required>';
+echo '</div>';
 
-            // Selección de hora disponible
-            echo '<div class="mb-3">';
-            echo '<label for="hora" class="form-label">Seleccionar Hora Disponible:</label>';
-            echo '<select class="form-select" id="hora" name="hora" required>';
-            echo '</div>';
-            
-            setlocale(LC_TIME, 'es_ES.UTF-8');
+echo '<div class="mb-3">';
+echo '<label for="cotizacion" class="form-label">Cotización:</label>';
+echo '<span id="cotizacion_aproximada"></span>';
+echo '</div>';
+echo '</div>';
 
-            // Consulta para obtener las horas disponibles
-            $sql_horas_disponibles = "SELECT * FROM horarios_disponibles WHERE usuario_id = $usuario_id AND estado = 'Disponible'";
-            $resultado_horas_disponibles = $conexion->query($sql_horas_disponibles);
-            
-            if ($resultado_horas_disponibles->num_rows > 0) {
-                while ($fila_hora = $resultado_horas_disponibles->fetch_assoc()) {
-                    // Formatear la fecha en el formato deseado en español
-                    $fecha_formateada = strftime('%d-%B-%Y', strtotime($fila_hora['fecha']));
-                    
-                    // Convertir el valor del turno a "Mañana" o "Tarde"
-                    $turno_texto = ($fila_hora['turno'] == 'am') ? 'Mañana' : 'Tarde';
-                    
-                    echo "<option value='{$fila_hora['id']}'>$fecha_formateada - $turno_texto</option>";
-                }
-            } else {
-                echo '<option value="" disabled>No hay horas disponibles</option>';
-            }
-            
-            echo '</select>';
-            echo '</div>';
 
-            echo '<button type="submit" class="btn btn-primary">Reservar cita</button>';
-            echo '</form>';
-            echo '</div>';
-            echo '</div>';
-            echo '</div>';
+echo '</div>';
+echo '</div>';
+
+// Selección de hora disponible
+echo '<div class="mb-3">';
+echo '<label for="hora" class="form-label">Seleccionar Hora Disponible:</label>';
+echo '<select class="form-select" id="hora" name="hora" required>';
+echo '</div>';
+
+setlocale(LC_TIME, 'es_ES.UTF-8');
+
+// Consulta para obtener las horas disponibles
+$sql_horas_disponibles = "SELECT * FROM horarios_disponibles WHERE usuario_id = $usuario_id AND estado = 'Disponible'";
+$resultado_horas_disponibles = $conexion->query($sql_horas_disponibles);
+
+if ($resultado_horas_disponibles->num_rows > 0) {
+    while ($fila_hora = $resultado_horas_disponibles->fetch_assoc()) {
+        // Formatear la fecha en el formato deseado en español
+        $fecha_formateada = strftime('%d-%B-%Y', strtotime($fila_hora['fecha']));
+
+        // Convertir el valor del turno a "Mañana" o "Tarde"
+        $turno_texto = ($fila_hora['turno'] == 'am') ? 'Mañana' : 'Tarde';
+
+        echo "<option value='{$fila_hora['id']}'>$fecha_formateada - $turno_texto</option>";
+    }
+} else {
+    echo '<option value="" disabled>No hay horas disponibles</option>';
+}
+
+echo '</select>';
+echo '</div>';
+
+echo '<button type="submit" class="btn btn-primary">Reservar cita</button>';
+echo '</form>';
+echo '</div>';
+echo '</div>';
+echo '</div>';
 
             
         } else {
