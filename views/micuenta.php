42df1655d3f57018d84e354e96e0705cd171e65b
Ceguerrerop <134183626+Ceguerrerop@users.noreply.github.com>
2024-05-01 21:48:36 -0400

    incroporacion de footer reajuste de dimensiones

--- views/micuenta.php
+++ views/micuenta.php
@@ -14,40 +14,44 @@ session_start();
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Studio de Tatuajes - Clientes</title>
   <link rel="stylesheet" href="../assets/css/style.css">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
 </head>
 
 <body class="fondo">
-  <div class="container">
-    <nav class="navbar navbar-expand-lg navbar-light">
-      <a class="navbar-brand" href="#"><img class="logo" src="../assets/img/logo.png" alt=""></a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon "></span>
-      </button>
+  <div>
+      <nav class="navbar navbar-expand-lg navbar-light ">
+          <a class="navbar-brand mr-auto" href="#">
+          <img class="logo" src="../assets/img/logo.png" alt=""></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon "></span>
+          </button>
+      
+          <div class="collapse navbar-collapse justify-content-end " id="navbarSupportedContent">
+            <ul class="navbar-nav mg-auto   nav-underline">
 
-      <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link btn" href="#">Nosotros</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link btn" href="tatuadores_view.php">Tatuadores</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link btn" href="#">Perforadores</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link btn" href="micuenta.php">Mis Reservas</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link btn" href="../index.php">Cerrar Sesion</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Nosotros</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="tatuadores_view.php">Tatuadores</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Perforadores</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href='/micuenta.php'>Mis reservas</a>
+              </li>
+              <li class="nav-item">
+                    <a class="nav-link btn" href="../index.php">Cerrar Sesion</a>
+              </li>
+              
+            </ul>
+          </div>
+        </nav>
 
     <?php 
 
@@ -131,18 +135,82 @@ session_start();
 
 
   </div>
+
+
+
+  <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-sm-6">
+                    <div class="single-box">
+                        <img src="img/logo.png" alt="">
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam repellendus sunt praesentium aspernatur iure molestias.</p>
+                        <h3></h3>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="single-box">
+                        <h2>Hosting</h2>
+                    <ul>
+                        <li><a href="#">Web Hosting</a></li>
+                        <li><a href="#">Cloud Hosting</a></li>
+                        <li><a href="#">CMS Hosting</a></li>
+                        <li><a href="#">WordPress Hosting</a></li>
+                        <li><a href="#">Email Hosting</a></li>
+                        <li><a href="#">VPS Hosting</a></li>
+                    </ul>
+                    </div>                    
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="single-box">
+                        <h2>Domain</h2>
+                    <ul>
+                        <li><a href="#">Web </a></li>
+                        <li><a href="#">Cloud</a></li>
+                        <li><a href="#">CMS </a></li>
+                        <li><a href="#">WordPress</a></li>
+                        <li><a href="#">Email</a></li>
+                        <li><a href="#">VPS</a></li>
+                    </ul>
+                    </div>                    
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="single-box">
+                        <h2>Newsletter</h2>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur doloremque earum similique fugiat nobis. Facere?</p>
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Enter your Email ..." aria-describedby="basic-addon2">
+                            <span class="input-group-text" id="basic-addon2"><i class="fa fa-long-arrow-right"></i></span>
+                        </div>
+                        <h2>Redes Sociales</h2>
+                        <p class="socials">
+                          <i class="bi bi-whatsapp"></i>
+                          <i class="bi bi-facebook"></i>
+                          <i class="bi bi-linkedin"></i>
+                          <i class="bi bi-instagram"></i>
+                        </p>
+                    </div>
+                    
+                </div>
+                
+            </div>
+        </div>
+
+        <div class="text-center py-3">
+          <p class="mb-0">&copy; 2024 TattoStudioINK</p>
+        </div>
+
+  </footer>
+
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+  integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
+  integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 </body>
 
-<footer class="bg-dark text-white  fixed-bottom">
-  <div class="text-center py-3 fondo">
-    <p class="mb-0">&copy; 2024 TattoStudioINK</p>
-  </div>
-</footer>
 
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
-  integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
-  integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+
+
 
 </html>
 
