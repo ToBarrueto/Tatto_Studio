7587bbb4d6741ea497e75f03c4a93139c65d5ba9
Tomas Barrueto <tobarrueto@gmail.com>
2024-05-05 00:12:21 -0400

    landing v2

--- views/landing.php
+++ views/landing.php
@@ -42,39 +42,121 @@
                 <div class="col">
                     <h1 class="title">TattoStudioINK</h1>
                     <p class="p-custom">
-                        "Nacemos desde el amor a la tinta, al arte y todos sus variantes, para poder regarlo por toda Santiago
-                        en general. Entregando trabajos de calidad y que perduren en el tiempo, nuestra pasión por la expresión
-                        artística nos impulsa a explorar nuevas formas, a innovar y a dejar una huella duradera en cada proyecto
-                        que emprendemos. Nos comprometemos a cultivar la creatividad, a inspirar a otros y a enriquecer el mundo
-                        que nos rodea con nuestras creaciones."</p>
-                  </div>
-                  
-                  
+                        "Nacemos desde el amor a la tinta, al arte y todos sus variantes, para poder regarlo por toda
+                        Santiago
+                        en general. Entregando trabajos de calidad y que perduren en el tiempo."</p>
+                    <button class="button-custom mt-5">Agenda tu Hora</button>
+                </div>
+
+
             </div>
         </div>
     </div>
 
-        <div>
-            <h3>Sobre nosotros</h3>
+    <div class="zona2 d-flex justify-content-center ">
+        <div class="container text-center ">
+            <p class="p-custom1">ESTUDIO DE TATUAJES Y PERFORACIONES</p>
+            <div class="row g-5">
+                <div class="col-sm-6 col-md-8">
+                    <p class="title-custom">Sobre Nosotros</p>
+                    <p class="p-custom2">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae eum facilis
+                        distinctio obcaecati
+                        tempore asperiores suscipit debitis, exercitationem animi molestiae officiis magnam est ipsa
+                        dolore vel magni nulla eaque accusantium iusto, nihil voluptatibus id, ducimus itaque similique.
+                        Laudantium culpa consequuntur</p>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col">
+                                <div class="container-icons">
+                                    <p class="p-icons">+4500 Tattos</p>
+                                    <img src="../assets/img/landing/machine.jpg" alt="">
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="container-icons">
+                                    <p class="p-icons">+500 Piercings</p>
+                                    <img src="../assets/img/landing/piercing.png" alt="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
+                <div class="col-6 col-md-4">
+                    <img class="imagen-custom p-2" src="../assets/img/landing/about2.jpg" alt="">
+                </div>
+            </div>
         </div>
+    </div>
+
+    <div class="zona3 d-flex justify-content-center ">
+        <div class="container text-center ">
+            <div class="row">
+                <div class="col">
+                    <h1 class="title-custom3">Nuestros Servicios</h1>
+                    <p class="p-custom3">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias voluptatibus porro soluta totam minima velit magni, provident earum. Quia dolorum recusandae voluptatem iure itaque provident cumque adipisci fugiat illum explicabo!</p>
+                </div>
+
+                <div class="container px-4 text-center">
+                    <div class="row gx-3">
+                        <div class="col">
+                            <div class="container-icons">
+                                <div class="card-giratoria">
+                                    <div class="card-inner">
+                                        <div class="card-front">
+                                            <img src="../assets/img/landing/card-tatto.jpg" alt="Imagen">
+                                        </div>
+                                        <div class="card-back">
+                                            <h2>TATUAJES</h2>
+                                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias voluptatibus porro soluta totam minima velit magni, provident earum. Quia dolorum recusandae voluptatem iure itaque provident cumque adipisci fugiat illum explicabo!
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col">
+                            <div class="container-icons">
+                                <div class="card-giratoria">
+                                    <div class="card-inner">
+                                        <div class="card-front">
+                                            <img src="../assets/img/landing/card-piercing.jpg" alt="Imagen">
+                                        </div>
+                                        <div class="card-back">
+                                            <h2>PIERCINGS</h2>
+                                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab, dolorem mollitia ad temporibus repudiandae cupiditate earum fugit explicabo ex provident corrupti itaque eos magni nesciunt in? Minus harum inventore doloribus!
+                                            Accusamus, rerum suscipit eligendi ea laboriosam aliquid omnis harum cumque, nihil earum eaque ducimus officia? Excepturi impedit beatae alias, ipsa ea voluptates, quam nobis molestiae minima asperiores, ut quaerat velit?</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-            crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
-            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
-            crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
-            integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
-            crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
+        integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
+        crossorigin="anonymous"></script>
 
-        <script type="text/javascript">
-            window.addEventListener("scroll", function () {
-                var header = document.querySelector("header");
-                header.classList.toggle("abajo", window.scrollY > 0);
-            })
-        </script>
+    <script type="text/javascript">
+        window.addEventListener("scroll", function () {
+            var header = document.querySelector("header");
+            header.classList.toggle("abajo", window.scrollY > 0);
+        })
+    </script>
 </body>
 
 </html>
