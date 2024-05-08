b2a81be28dc9170c1141fa2c378dac2082d5ee06
Tomas Barrueto <tobarrueto@gmail.com>
2024-05-07 21:20:48 -0400

    cambios menores

--- views/landing.php
+++ views/landing.php
@@ -11,7 +11,7 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Landing</title>
+    <title>TattoStudioINK</title>
     <link rel="stylesheet" href="../assets/css/landing.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
